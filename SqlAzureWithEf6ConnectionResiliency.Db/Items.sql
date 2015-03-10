97d848c68d28374d7e3cbafb9cbd4b0ee926eb5c
dan@budimir.com <dan@budimir.com>
2015-03-10 12:30:00 -0400

    Initial

---
+++ SqlAzureWithEf6ConnectionResiliency.Db/Items.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Items]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Field1] VARCHAR(50) NULL, 
+    [Field2] INT NULL DEFAULT 0, 
+    [Field3] DATE NULL DEFAULT getdate()
+)
---
+++ SqlAzureWithEf6ConnectionResiliency/Model/ModelContext.cs
@@ -0,0 +1,24 @@
+namespace SqlAzureWithEf6ConnectionResiliency.Model
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class ModelContext : DbContext
+    {
+        public ModelContext()
+            : base("name=ModelContext")
+        {
+        }
+
+        public virtual DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>()
+                .Property(e => e.Field1)
+                .IsUnicode(false);
+        }
+    }
+}
