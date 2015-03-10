a44e4fac4cb6416d28786e064ed492a76444e545
dan@budimir.com <dan@budimir.com>
2015-03-10 12:47:41 -0400

    enhance

--- SqlAzureWithEf6ConnectionResiliency/Model/SqlAzureExecutionStrategyDbConfig.cs
+++ SqlAzureWithEf6ConnectionResiliency/Model/SqlAzureExecutionStrategyDbConfig.cs
@@ -11,8 +11,17 @@ namespace SqlAzureWithEf6ConnectionResiliency.Model
     class SqlAzureExecutionStrategyDbConfig :  DbConfiguration 
     {
         public SqlAzureExecutionStrategyDbConfig() 
-        { 
-            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy()); 
+        {
+            //     The default retry limit is 5, which means that the total amount of time spent
+            //     between retries is 26 seconds plus the random factor.
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+
+
+            // could choose to configure here
+            //int maxRetries = 5;
+            //TimeSpan maxWait = TimeSpan.FromSeconds(30);
+            //SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy(maxRetries,maxWait)); 
+
         } 
     }
 }
